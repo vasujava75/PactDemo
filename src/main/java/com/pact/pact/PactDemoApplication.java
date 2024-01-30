b247fae8d3fa9718d2d3aa6a1d0af2451028acc4
HE06BW <vasudev.kondapeti@ing.com>
2024-01-30 15:10:22 +0100

    first repo

---
+++ src/main/java/com/pact/pact/PactDemoApplication.java
@@ -0,0 +1,13 @@
+package com.pact.pact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PactDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PactDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pact/pact/PactDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pact.pact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PactDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
